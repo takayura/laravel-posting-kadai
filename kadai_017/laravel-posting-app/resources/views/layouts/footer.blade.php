c05209082b69eff9d799f3c75d6632340e3acda0
鷹羽ゆら <yurayingyu@gmail.com>
2024-06-03 22:35:01 +0900

    Delete kadai_017/laravel-posting-app directory

--- kadai_017/laravel-posting-app/resources/views/layouts/footer.blade.php
+++
@@ -1,3 +0,0 @@
-<footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">
-    <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
-</footer>
